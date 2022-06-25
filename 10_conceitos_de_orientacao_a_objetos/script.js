568b7cd7afc99cb10e576a18b52aa803f081f2b5
Gabriel Gelbcke <gabrielgelbcke@gmail.com>
2022-06-25 00:39:46 -0300

    atualização da aula com adição de conteúdos

--- 10_conceitos_de_orientacao_a_objetos/script.js
+++ 10_conceitos_de_orientacao_a_objetos/script.js
@@ -1,4 +1,4 @@
-/* MÉTODOS.
+// MÉTODOS.
 let cachorro = {
     pata: 4,
     genero: "masculino",
@@ -77,7 +77,7 @@ Maratonista.prototype.correr = function () {
 let corredor = new Maratonista("homem")
 
 corredor.correr();
-console.log(corredor.sexo)*/
+console.log(corredor.sexo)
 
 // CONSTRTUTOR NA CLASSE (ATUALIZAÇÃO PARA ES6).
 class Smartphone {
@@ -92,3 +92,69 @@ console.log(apple.marca)
 Smartphone.prototype.marca = "Sem marca";
 console.log(apple.marca);
 console.log(Smartphone.prototype.marca);
+
+// MÉTODOS E PROPRIEDADES EM UMA CLASS.
+class urso {
+    constructor (raca) {
+        this.raca = raca;
+    }
+    comer() {
+        console.log('Comer banana');
+    }
+}
+
+urso.prototype.patas = 4;
+
+let panda = new urso('panda', 'preto e branco');
+
+console.log(panda.patas)
+
+// SYMBOLS.
+let pesoMedio = Symbol();
+urso.prototype[pesoMedio] = 95;
+
+console.log(urso.prototype[pesoMedio] + "kg"); // acessando o symbol - apenas via prototype.
+
+// GETTERS AND SETTERS.
+class garrafa {
+    constructor(tipo) {
+        this.tipo = tipo;
+    }
+    get verTipo() {
+        return 'O tipo desta garrafa é de ' + this.tipo;
+    }
+    set novoTipo(value) {
+        this.tipo = value;
+    }
+}
+
+let plastica = new garrafa ('plástico.')
+console.log(plastica.verTipo)
+
+garrafa.novoTipo = "vidro.";
+console.log(garrafa.verTipo)
+
+// HERANÇA.
+class mamifero {
+    constructor(patas) {
+        this.patas = patas;
+    }
+}
+
+let coelho = new mamifero(4);
+console.log(coelho);
+
+class animaisSelvagens extends mamifero { // extends é utilizado para dar herança a uma classe, herdando suas propriedades.
+    constructor (patas, especie, cor) {
+        super(patas, patas) // super é utilizado para acessas as propriedades da classe pai.
+        this.especie = especie;
+        this.cor = cor;
+    }
+}
+
+let coiote = new animaisSelvagens(4, "coiote", "cinza")
+console.table(coiote)
+
+// INSTANCE OF.
+console.log(new animaisSelvagens instanceof mamifero) // utilizado para identificar quem é o pai do objeto.
+console.log(new mamifero instanceof animaisSelvagens)
